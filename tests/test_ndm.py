4c14f774bfc90e1241e71b8b2a9a42045d0e6820
jtgasparik <jgasparik@bnl.gov>
2026-02-17 14:54:41 -0500

    added flag to centralized_derivative function to normalize the derivative

--- tests/test_ndm.py
+++ tests/test_ndm.py
@@ -5,7 +5,7 @@ def test_central_difference():
     my_sp2b = pysp2.io.read_sp2(pysp2.testing.EXAMPLE_SP2B)
     my_ini = pysp2.io.read_config(pysp2.testing.EXAMPLE_INI)
     my_binary = pysp2.util.gaussian_fit(my_sp2b, my_ini, parallel=False)
-    dSdt = pysp2.util.central_difference(my_binary)
+    dSdt = pysp2.util.central_difference(my_binary, normalize=False)
     
     np.testing.assert_almost_equal(dSdt['Data_ch4'].isel(event_index=5876, time=0).item(), 
                                    8.3333333333e6, decimal=2)
@@ -14,3 +14,11 @@ def test_central_difference():
     np.testing.assert_almost_equal(dSdt['Data_ch4'].isel(event_index=5876, time=19).item(), 
                                    1.5e7, decimal=2)
     assert np.isfinite(dSdt).all()
+
+    dSdt_norm = pysp2.util.central_difference(my_binary, normalize=True)
+    np.testing.assert_almost_equal(dSdt_norm['Data_ch4'].isel(event_index=5876, time=0).item(), 
+                                   8.3333333333e6/-30168, decimal=2)
+    np.testing.assert_almost_equal(dSdt_norm['Data_ch4'].isel(event_index=5876, time=99).item(), 
+                                   7.166666666e7/-30152, decimal=2)
+    np.testing.assert_almost_equal(dSdt_norm['Data_ch4'].isel(event_index=5876, time=19).item(), 
+                                   1.5e7/-30132, decimal=2)
