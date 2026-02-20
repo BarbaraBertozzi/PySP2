d9e270b29c2356ba59ee4a481b659ed6b83c44b6
jtgasparik <jgasparik@bnl.gov>
2026-02-20 13:45:05 -0500

    added vis tests file

---
+++ tests/test_vis.py
@@ -0,0 +1,20 @@
+import matplotlib
+import pytest
+import xarray as xr
+import numpy as np
+
+import pysp2
+from pysp2.util.normalized_derivative_method import plot_normalized_derivative
+
+@pytest.mark.mpl_image_compare(tolerance=10)
+def test_plot_normalized_derivative():
+
+    my_sp2b = pysp2.io.read_sp2(pysp2.testing.EXAMPLE_SP2B)
+    my_ini = pysp2.io.read_config(pysp2.testing.EXAMPLE_INI)
+    my_binary = pysp2.util.gaussian_fit(my_sp2b, my_ini, parallel=False)
+    dSdt_norm = pysp2.util.central_difference(my_binary, normalize=True)
+
+    # Test the plotting function for channel 0 and record number 2
+    ax = plot_normalized_derivative(dSdt_norm, record_no=2, chn=0)
+    
+    return ax
